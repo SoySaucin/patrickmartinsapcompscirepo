2f537f1565c60305963fbba49425a586feb3c02e
thehairyrock@gmail.com <thehairyrock@gmail.com@6c81211a-07f4-cbcb-5116-17ffa12db716>
2012-02-01 17:24:25 +0000

    Unit0 Commit 2

---
+++ trunk/Unit0/src/Unit0/lab0a/Lab0a2.java
@@ -0,0 +1,39 @@
+package Unit0.lab0a;
+
+//� A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class Lab0a2
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Patrick Martin \t  2/1/2012 \n\n" );
+                for(int i=0; i<2;i++){
+                    System.out.print("+++++++++++++++++++++++++++\n" );
+                }
+                for(int i=0; i<4;i++){
+                    System.out.print("+++\t\t\t+++\n" );
+                }
+                System.out.print("+++\tCompSci\t\t+++\n" );
+                for(int i=0; i<4;i++){
+                    System.out.print("+++\t\t\t+++\n" );
+                }
+                for(int i=0; i<2;i++){
+                    System.out.print("+++++++++++++++++++++++++++\n" );
+                }
+
+
+
+
+
+
+
+
+
+
+
+	}
+}
---
+++ trunk/Unit0/src/Unit0/lab0a/Lab0a3.java
@@ -0,0 +1,28 @@
+package Unit0.lab0a;
+
+//� A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class Lab0a3
+{
+	public static void main ( String[] args )
+	{
+		System.out.println("Patrick Martin \t  2/1/2012 \t 1\n\n" );
+		System.out.println("Rabbit" );
+		System.out.println("\n\n\n\n" );
+
+
+		System.out.println("\t() ()");
+                System.out.println("\t( . .)");
+                System.out.println("\tc(\")(\")\n");
+		//add other output
+
+		//System.out.println(" \n\n\n\nHelpFul Hints" );
+		//System.out.println("\\\\ draws one backslash on the screen!\n" );
+		//System.out.println("\\\" draws one double quote on the screen!\n" );
+		//System.out.println("\\\' draws one single quote on the screen!\n" );
+	}
+}
