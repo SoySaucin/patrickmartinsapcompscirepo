4bdeba0c0a8b61fdcaa3a9e22884763896cccc86
thehairyrock <thehairyrock@gmail.com>
2012-02-01 16:54:56 +0000

    Unit0 Commit 1

---
+++ Unit0/Labs/lab0aGW_actor/ActorTriangle.java
@@ -0,0 +1,32 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import info.gridworld.actor.Actor;
+import info.gridworld.grid.Location;
+import info.gridworld.actor.ActorWorld;
+
+public class ActorTriangle
+{
+	public static void main(String[] args)
+	{
+		//world?
+		
+		
+		//add actors to make a triangle
+		
+		
+		
+		//left and right side of the triangle
+
+
+		
+		
+		//bottom row
+
+
+
+	}
+}
---
+++ Unit0/Slides-ExampleCode/0a.syntax_output/Two.java
@@ -0,0 +1,14 @@
+//© A+ Computer Science
+// www.apluscompsci.com
+
+//print/println example
+
+public class Two
+{
+	public static void main(String args[])
+	{
+		System.out.println("c\tompsci");
+		System.out.println("com\tpsci");
+		System.out.println("comp\nsci");
+	}	
+}
