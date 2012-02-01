b23549cfe72da4d126c7f325d83810ece0f43e73
thehairyrock <thehairyrock@gmail.com>
2012-02-01 17:24:25 +0000

    Unit0 Commit 2

---
+++ Unit0/src/Unit0/lab0a/Lab0a1.java
@@ -0,0 +1,54 @@
+package Unit0.lab0a;
+
+//� A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class Lab0a1
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Patrick Martin \t  2/1/2012 \n\n" );
+		System.out.print("+++++++++++++++++++++++++\n" );
+                System.out.print("+++++++++++++++++++++++++\n" );
+                System.out.print("+++++++++++++++++++++++++\n" );
+                System.out.print("=========================\n" );
+                System.out.print("=========================\n" );
+                System.out.print("+++++++++++++++++++++++++\n" );
+                System.out.print("+++++++++++++++++++++++++\n" );
+                System.out.print("+++++++++++++++++++++++++\n" );
+                System.out.print("=========================\n" );
+                System.out.print("=========================\n" );
+                System.out.print("+++++++++++++++++++++++++\n" );
+                System.out.print("+++++++++++++++++++++++++\n" );
+                System.out.print("+++++++++++++++++++++++++\n" );
+                System.out.print("\n\n" );
+                for(int i=0; i<3;i++){
+                    System.out.print("+++++++++++++++++++++++++\n" );
+                }
+                for(int i=0; i<2;i++){
+                    System.out.print("=========================\n" );
+                }
+                for(int i=0; i<3;i++){
+                    System.out.print("+++++++++++++++++++++++++\n" );
+                }
+                for(int i=0; i<2;i++){
+                    System.out.print("=========================\n" );
+                }
+                for(int i=0; i<3;i++){
+                    System.out.print("+++++++++++++++++++++++++\n" );
+                }
+
+
+
+
+
+
+
+
+
+
+	}
+}
