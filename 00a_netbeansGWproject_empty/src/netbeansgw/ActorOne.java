1c82352e5b2806587ea49e0d234e059d0f8aa8ab
thehairyrock <thehairyrock@gmail.com>
2012-02-01 17:29:51 +0000

    GridWorld Commit 1

---
+++ 00a_netbeansGWproject_empty/src/netbeansgw/ActorOne.java
@@ -0,0 +1,18 @@
+package netbeansgw;
+// A+ Computer Science
+// www.apluscompsci.com
+
+import info.gridworld.actor.Actor;
+import info.gridworld.grid.Location;
+import info.gridworld.actor.ActorWorld;
+
+public class ActorOne
+{
+   public static void main(String[] args)
+   {
+      ActorWorld world = new ActorWorld();
+      Actor dude = new Actor();
+      world.add(new Location(0,0), dude);
+      world.show();       
+   }
+}
