3cf0bbf93555022ea03d5919e44af5f819d04896
thehairyrock@gmail.com <thehairyrock@gmail.com@6c81211a-07f4-cbcb-5116-17ffa12db716>
2012-02-01 17:29:51 +0000

    GridWorld Commit 1

---
+++ trunk/00a_netbeansGWproject_empty/src/netbeansgw/Main.java
@@ -0,0 +1,33 @@
+// A+ Computer Science
+// www.apluscompsci.com
+
+/*
+    Copy and paste all gridworld .java source files to the src folder
+
+    Click on Run - Clean and Build Project to get everything setup
+
+    F9 will compile your individual .java file!
+    Shift + F6 will run your compiled class file!
+    You can also right-click your file and run your class file.
+ */
+
+
+package netbeansgw;
+
+/**
+ *
+ * @author stacey.armstrong
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("F9 will compile your file!");
+        System.out.println("Shift + F6 will run your file!");
+
+        System.out.println("You can also right-click your file and run it.");
+    }
+}
