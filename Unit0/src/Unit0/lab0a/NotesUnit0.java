ae4802a6691ff13e7bd8e263c9e6b074dd62b250
thehairyrock <thehairyrock@gmail.com>
2012-02-02 17:28:18 +0000

    Unit0 Commit 2

---
+++ Unit0/src/Unit0/lab0a/NotesUnit0.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Unit0.lab0a;
+
+/**
+ *
+ * @author martinp4325
+ */
+public class NotesUnit0 {
+    //java syntax
+    public static void main(String[]args){
+        System.out.print("java is cool!");
+    }//end main
+    
+}//end class
---
+++ Unit0/src/Unit0/lab0b/Lab0b.java
@@ -0,0 +1,63 @@
+package Unit0.lab0b;
+
+//� A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class Lab0b
+{
+	public static void main ( String[] args )
+	{
+		//define 1 variable of each of the
+		//following data types
+		//byte		short		int 		long
+		//float		double
+		//char      boolean		String
+
+		//integer variables
+		byte byteOne = 127;
+                short shortOne = 42;
+                int integerOne = 9001;
+                long longOne = 999999999;
+                //long longlongOne[]={999999999,999999999};
+		//decimal variables
+
+
+		//other integer types
+
+
+		//other types
+
+
+		//output your information here
+		System.out.println("////////////////////////////////////////");
+		System.out.println("*Patrick Martin                08/18/06 *");
+		System.out.println("*                                       *");
+		System.out.println("*            integer types              *");
+		System.out.println("*                                       *");
+		System.out.println("*8 bit - byteOne = "+byteOne+"\t\t\t*");
+                System.out.println("*16 bit - shortIntegerOne = "+shortOne+"\t\t*");
+                System.out.println("*32 bit - integerOne = "+integerOne+"\t\t*");
+                System.out.println("*64 bit - longOne = "+longOne+"\t\t*");
+               // System.out.println("*128 bit - longlongOne = "+(longlongOne[0]+longlongOne[1])+"\t\t*");
+                System.out.println("*                                       *");
+		System.out.println("*            decimal types              *");
+		System.out.println("*                                       *");
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
+	}
+}
