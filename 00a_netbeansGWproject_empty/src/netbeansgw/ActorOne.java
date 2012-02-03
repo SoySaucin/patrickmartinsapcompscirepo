72687c2b19bc6c7fcd4bf14582b9c33b1d3968b2
thehairyrock <thehairyrock@gmail.com>
2012-02-03 17:30:53 +0000

    Gridworld intital work.

--- 00a_netbeansGWproject_empty/src/netbeansgw/ActorOne.java
+++ 00a_netbeansGWproject_empty/src/netbeansgw/ActorOne.java
@@ -5,6 +5,8 @@ package netbeansgw;
 import info.gridworld.actor.Actor;
 import info.gridworld.grid.Location;
 import info.gridworld.actor.ActorWorld;
+import info.gridworld.actor.Bug;
+import info.gridworld.actor.Critter;
 
 public class ActorOne
 {
@@ -13,6 +15,16 @@ public class ActorOne
       ActorWorld world = new ActorWorld();
       Actor dude = new Actor();
       world.add(new Location(0,0), dude);
+      
+      Bug bob = new Bug();
+      world.add(new Location(1,1), bob);
+      
+      Critter joe = new Critter();
+      world.add(new Location(5,5), joe);
+      Critter stan = new Critter();
+      world.add(new Location(9,9), stan);
+      
       world.show();       
+      dude.act();
    }
 }
