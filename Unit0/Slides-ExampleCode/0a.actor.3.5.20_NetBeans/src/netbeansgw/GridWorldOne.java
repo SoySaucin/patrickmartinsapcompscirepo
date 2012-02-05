5ad86c42591ae2ba4cae6f1096cafc5c5d3bcec7
thehairyrock <thehairyrock@gmail.com>
2012-02-05 23:56:32 +0000

    HW weekend 1

---
+++ Unit0/Slides-ExampleCode/0a.actor.3.5.20_NetBeans/src/netbeansgw/GridWorldOne.java
@@ -0,0 +1,15 @@
+package netbeansgw;
+
+//� A+ Computer Science 
+// www.apluscompsci.com
+
+import info.gridworld.actor.ActorWorld;
+
+public class GridWorldOne
+{
+   public static void main(String[] args)
+   {
+      ActorWorld earth = new ActorWorld();
+      earth.show();       
+   }
+}
