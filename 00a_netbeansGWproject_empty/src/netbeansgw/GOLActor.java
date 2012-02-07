b5407161af2c1fb90c7e1ab2e541ab267158164c
thehairyrock <thehairyrock@gmail.com>
2012-02-07 16:16:24 +0000

    HW weekend 1

--- 00a_netbeansGWproject_empty/src/netbeansgw/GOLActor.java
+++ 00a_netbeansGWproject_empty/src/netbeansgw/GOLActor.java
@@ -20,17 +20,18 @@ public class GOLActor extends Actor {
         gridworld = world;
     }
     
+    @Override
     public void act(){
-        if(this.getGrid().getNeighbors(this.getLocation()).size()<2){
-            this.removeSelfFromGrid();
-            return;
-        }
         for (Iterator<Location> iter = this.getGrid().getEmptyAdjacentLocations(this.getLocation()).iterator(); iter.hasNext();){
             Location loc = iter.next();
             if (this.getGrid().getNeighbors(loc).size()==3){
                 GOLActor NewGOL =new GOLActor(gridworld);
                 gridworld.add(loc,NewGOL);
             }
+        }
+        if(this.getGrid().getNeighbors(this.getLocation()).size()<2){
+            this.removeSelfFromGrid();
+            return;
         }
         if(this.getGrid().getNeighbors(this.getLocation()).size()>3){
             this.removeSelfFromGrid();
