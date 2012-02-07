153dde715b6af21cc4e468bb1d47916664fd08f5
thehairyrock <thehairyrock@gmail.com>
2012-02-07 17:34:12 +0000

    Unit 1 inital commit

---
+++ Unit1/src/unit1/lab1a/SmileyFace.java
@@ -0,0 +1,44 @@
+package unit1.lab1a;
+
+//� A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import java.awt.Graphics;
+import java.awt.Color;
+import java.awt.Canvas;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class SmileyFace extends Canvas
+{
+   public SmileyFace()    //constructor - sets up the class
+   {
+      setSize(800,600);
+      setBackground(Color.WHITE);
+      setVisible(true);
+   }
+
+   public void paint( Graphics window )
+   {
+      smileyFace(window);
+   }
+
+   public void smileyFace( Graphics window )
+   {
+      window.setColor(Color.BLUE);
+      window.drawString("SMILEY FACE LAB ", 35, 35);
+
+      window.setColor(Color.YELLOW);
+      window.fillOval( 210, 100, 400, 400 );
+      window.setColor(Color.BLACK);
+      window.fillArc((int)362.5, 300, 100, 50, 180, 180);
+      window.fillArc(350, 275, 125, 50, 0, 180);
+      
+		//add more code here
+
+
+   }
+}
