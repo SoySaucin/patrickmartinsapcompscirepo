db63ae20b261814600f4665ec193369216a3ff30
thehairyrock <thehairyrock@gmail.com>
2012-02-09 15:57:26 +0000

    Unit 2 inital commit

---
+++ Unit2/src/u2/NotesUnit2.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package u2;
+
+/**
+ * Description:
+ * DateCreated:
+ *
+ * @author martinp4325
+ */
+public class NotesUnit2 {
+    
+    Student test = new Student("Test",1,1);
+}
