04965eda1f794778d2a6b0fe1ccc47a17e408f4c
thehairyrock@gmail.com <thehairyrock@gmail.com@6c81211a-07f4-cbcb-5116-17ffa12db716>
2012-02-09 15:57:26 +0000

    Unit 2 inital commit

---
+++ trunk/Unit2/src/u2/Student.java
@@ -0,0 +1,63 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package u2;
+
+/**
+ * Description:
+ * DateCreated:
+ *
+ * @author martinp4325
+ */
+public class Student {
+    private String name;
+    private double  age;
+    private long studentID;
+    private double[][] grades;
+    
+    
+    public Student(){
+        this.name = "default";
+        this.age = 13;
+        this.studentID =0;
+        
+    }
+    
+    public Student(String n,double a/* = 13*/,long ID/*=0*/){
+        this.name = n;
+        this.age = a;
+        this.studentID =ID;
+        
+    }
+    public Student(String n,int a/* = 13*/,int ID/*=0*/){
+        this.name = n;
+        this.age = a;
+        this.studentID =ID;
+        
+    }
+    public Student(String n,double a/* = 13*/,int ID/*=0*/){
+        this.name = n;
+        this.age = a;
+        this.studentID =ID;
+        
+    }
+    public Student(String n,int a/* = 13*/,long ID/*=0*/){
+        this.name = n;
+        this.age = a;
+        this.studentID =ID;
+        
+    }
+    
+    public double getGPA(){
+        double classgrade=0;
+        double grade = 0;
+        for(int i=0;i<grades.length;i++){
+            for (int n=0;n<grades[i].length;i++){
+                classgrade +=grades[i][n];
+            }
+            grade += classgrade/grades[i].length;
+        }
+        return grade/grades.length;
+    }
+}
